80925776d83727048b8c49ba614114443ac2ae33
CB_ATL_113_Smile_Rodnez <cb_atl_113_smile_rodnez@MacBook-Air-9.local>
2018-10-11 20:38:18 -0400

    Switching to Jeopardy

--- app.js
+++ app.js
@@ -1,44 +1,70 @@
-//First and foremost I need a away to get the word that the player wants to use
-var playtime = prompt("Please enter the word you want to guess", "enter word here")
+// trapped.addEventListener("load", playtime);
+// //First and foremost I need a away to get the word that the player wants to use
+// var playtime = prompt("Please enter the word you want to guess", "enter word here")
 
-//this if statement will allow the player to start playing the game after entering or not entering a word.
-if (playtime == null || playtime == "") {
-    // if the player does not enter a word or leaves the prompt box blank the game will not play
-    txt = "Come back when you want to play"
-    console.log(txt)
-} else {
-    //if the player enters a word the game begins
-    txt = "Let's play!"
-    console.log(txt)
-}
+// //this if statement will allow the player to start playing the game after entering or not entering a word.
+// if (playtime == null || playtime == "") {
+//     // if the player does not enter a word or leaves the prompt box blank the game will not play
+//     txt = "Come back when you want to play"
+//     console.log(txt)
+// } else {
+//     //if the player enters a word the game begins
+//     txt = "Let's play!"
+//     console.log(txt)
+// }
 
-//this is the word that the player has inputted and we will use to play the game
-console.log(playtime)
-
-
-// A want to be able to see the word split up into an array of characters
-var playtimeArray = playtime.split('')
-console.log(playtime)
+// //this is the word that the player has inputted and we will use to play the game
+// console.log(playtime)
 
 
-//I need a space for the word to be revealed, thi
-var answer = []
-for (var i = 0; i < playtimeArray.length; i++) {
-    answer[i] = "_ "
-}
-// what the frick
-function great() {
-    $('ul').append(answer)
-}
+// // A want to be able to see the word split up into an array of characters
+// var playtimeArray = playtime.split('')
+// console.log(playtimeArray)
 
 
-// I need an area to show which letters have been used 
-var usedLetters = []
+// //I need a space for the word to be revealed, thi
+// var answer = []
+// for (var i = 0; i < playtimeArray.length; i++) {
+//     answer[i] = "_"
+// }
+// // what the frick!!
+// function great() {
+//       $('ul').append(`${answer}`)
+// //    $('ul').append(answer)
+// }
+// console.log(great())
 
 
 
-// if all the balloons pop i want it to have an alert box that plays the sound file
-function lose() {
-    console.log('Mario fell! Oh no!')
+// // I need an area to show which letters have been used 
+// var usedLetters = []
 
-}
+
+
+// // if all the balloons pop i want it to have an alert box that plays the sound file
+// function lose() {
+//     console.log('Mario died! Oh no!')
+
+// } 
+// //create prompt
+// // var myPrompt = prompt("This is a prompt")
+// // //confirmed that it works
+// // console.log(myPrompt)
+
+
+// // var word = ['t','e','e','t']
+
+// // var selectedLetter = 'b'
+// // //put into another variable but this is unnecessary
+// // var userWord = myPrompt
+
+// // var userWordArray = userWord.split('')
+
+
+// // for(var i = 0; i < userWordArray.length; i++){
+// //     if(selectedLetter == userWordArray[i]){
+// //         document.getElementById('testing').innerHTML = userWordArray.join('')
+// //     }
+// // }
+// STOPPED TRYING TO DO HANGMAN SWITCHING TO JEOPARDY
+
